79ae94d33d9e23846a01bb4ba03cceae9d31a221
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-04-17 13:18:50 +0300

    Added Models And Migrations For Insurance,Organizations,Patients And Their Debts

--- database/migrations/2023_04_17_095945_create_organizations_table.php
+++ database/migrations/2023_04_17_095945_create_organizations_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Insurance;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('insurance_debts', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Insurance::class,'insurance_id');
-            $table->integer('amount');
-            $table->date('date');
+            $table->text('name');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('insurance_debts');
+        Schema::dropIfExists('organizations');
     }
 };
