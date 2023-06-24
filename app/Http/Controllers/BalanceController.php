f81cc85e82e033c2e066cff572277f90be428e34
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2023-06-24 19:13:44 +0300

    Added Validation For Balance

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -3,46 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\GetBalance\GetBalance;
+use App\Http\Requests\Balance\BalanceRequest;
 use Illuminate\Http\Request;
 
 class BalanceController extends Controller
 {
-    public function GetBalance(Request $request)
+    public function GetBalance(BalanceRequest $request)
     {
         $result = GetBalance::GetBalance($request,$request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetProfit(Request $request)
+    public function GetProfit(BalanceRequest $request)
     {
         $result = GetBalance::GetProfit($request,$request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetLoses(Request $request)
+    public function GetLoses(BalanceRequest $request)
     {
         $result = GetBalance::GetLoses($request,$request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetPatientTestsProfit(Request $request)
+    public function GetPatientTestsProfit(BalanceRequest $request)
     {
         $result = GetBalance::GetPatientTestsProfit($request,$request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetPunishmentsProfit(Request $request)
+    public function GetPunishmentsProfit(BalanceRequest $request)
     {
         $result = GetBalance::GetPunishmentsProfit($request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetSalaryLoses(Request $request)
+    public function GetSalaryLoses(BalanceRequest $request)
     {
         $result = GetBalance::GetSalaryLoses();
 
@@ -56,7 +57,7 @@ class BalanceController extends Controller
         return $result;
     }
 
-    public function GetRewardsLoses(Request $request)
+    public function GetRewardsLoses(BalanceRequest $request)
     {
         $result = GetBalance::GetRewardsLoses($request->from_date,$request->to_date);
 
@@ -70,28 +71,28 @@ class BalanceController extends Controller
         return $result;
     }
 
-    public function GetDebtLoses(Request $request)
+    public function GetDebtLoses(BalanceRequest $request)
     {
         $result = GetBalance::GetDebtLoses($request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetPatientDebtsLoses(Request $request)
+    public function GetPatientDebtsLoses(BalanceRequest $request)
     {
         $result = GetBalance::GetPatientDebtsLoses($request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetInsuranceDebtsLoses(Request $request)
+    public function GetInsuranceDebtsLoses(BalanceRequest $request)
     {
         $result = GetBalance::GetInsuranceDebtsLoses($request->from_date,$request->to_date);
 
         return $result;
     }
 
-    public function GetOrganizationDebtsLoses(Request $request)
+    public function GetOrganizationDebtsLoses(BalanceRequest $request)
     {
         $result = GetBalance::GetOrganizationDebtsLoses($request->from_date,$request->to_date);
 
